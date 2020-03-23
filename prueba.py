afb8a5e4b2178205938f5007c52aa03b0bdc05c7
Luisillo Programación <Luisillo@MacBook-Pro-de-LUIS.local>
2020-03-23 11:42:33 +0100

    Funciones clásicas

--- prueba.py
+++ prueba.py
@@ -5,4 +5,12 @@ def sumaTodos(limiTo):
         
     return resultado
 
+def sumaTodosLosCuadrados(limiTo):
+    resultado = 0
+    for i in range(limiTo+1):
+        resultado += i*i
+        
+    return resultado
+
 print(sumaTodos(100))
+print(sumaTodosLosCuadrados(3))
